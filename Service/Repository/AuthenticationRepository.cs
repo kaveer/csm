f60af541f476533d30cb0b198e0dd7612d1106d7
kaveer <kaveer.rajcoomar@sdworx.com>
2021-03-01 09:51:36 +0400

    Add authentication repository

---
+++ Service/Repository/AuthenticationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Repository
+{
+    public class AuthenticationRepository : IAuthenticationRepository
+    {
+    }
+}
