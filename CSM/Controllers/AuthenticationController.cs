f34373d095f5c652346bb582bb28ab0d21266125
kaveer <kaveer.rajcoomar@sdworx.com>
2021-03-01 09:49:11 +0400

    Add authentication copntroller

---
+++ CSM/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : Controller
+    {
+        [HttpGet("test")]
+        public ActionResult Index()
+        {
+            return Ok("test");
+        }
+    }
+}
---
+++ Model/Authentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Authentication
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
